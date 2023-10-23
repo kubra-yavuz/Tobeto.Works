c92c6cf003f70b9a90ba1bbbdb25083a869754ab
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 00:43:40 +0300

    C# Dersleri 10: Char Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,6 +4,7 @@
     {
         // Console.WriteLine("Hello, World!");
 
+        char character = 'A';
         bool condition = false;
         byte number4 = 255;
         short number3 = 32767;
@@ -13,7 +14,7 @@
         Console.WriteLine("Number2 is {0}", number2);
         Console.WriteLine("Number3 is {0}", number3);
         Console.WriteLine("Number4 is {0}", number4);
-
+        Console.WriteLine("Character is : {0}", (int) character);
         Console.ReadLine();
     }
 }
