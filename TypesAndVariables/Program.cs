933e6365201eb88b5879f548a21e9b31cb4eb511
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 00:37:27 +0300

    C# Dersleri 9: Bool Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,6 +4,7 @@
     {
         // Console.WriteLine("Hello, World!");
 
+        bool condition = false;
         byte number4 = 255;
         short number3 = 32767;
         int number1 = 2147483647;
