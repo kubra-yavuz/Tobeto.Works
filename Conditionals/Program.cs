832847655a0458495739d9914def3ee54bc91e0b
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 02:12:29 +0300

    C# Dersleri 15: If Bloklarıyla Çalışmaya Başlamak

---
+++ Conditionals/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var number = 11;
+if (number==10)
+{
+    Console.WriteLine("Number is 10");
+
+}
+
+else
+{
+    Console.WriteLine("Number is not 10");
+}
+
+    Console.ReadLine();
