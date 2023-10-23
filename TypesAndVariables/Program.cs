882c12d5e238d8d8fe5ca62b2617284b538f5198
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 00:21:08 +0300

    C# Dersleri 5: Integer Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -2,7 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Console.WriteLine("Hello, World!");
+
+        int number1 = 2147483647;
+        Console.WriteLine("Number1 is {0}",number1);
         Console.ReadLine();
     }
 }
