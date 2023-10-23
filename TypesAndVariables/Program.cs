3806f33e8c1fe8342a2e5a6579e2ea2e6689f73a
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 00:34:20 +0300

    C# Dersleri 8: Byte Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -4,12 +4,14 @@
     {
         // Console.WriteLine("Hello, World!");
 
+        byte number4 = 255;
         short number3 = 32767;
         int number1 = 2147483647;
         long number2 = -9223372036854775807;
         Console.WriteLine("Number1 is {0}", number1);
         Console.WriteLine("Number2 is {0}", number2);
         Console.WriteLine("Number3 is {0}", number3);
+        Console.WriteLine("Number4 is {0}", number4);
 
         Console.ReadLine();
     }
