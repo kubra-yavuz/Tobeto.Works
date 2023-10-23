9b0870b8a1cd14adc3f0e120d7a887de16a351f7
Kubra Yavuz <nurkacm@gmail.com>
2023-10-24 00:53:52 +0300

    C# Dersleri 12: Decimal Veri Tipi

--- TypesAndVariables/Program.cs
+++ TypesAndVariables/Program.cs
@@ -5,6 +5,7 @@
         // Console.WriteLine("Hello, World!");
 
         double number5 = 10.4;
+        decimal number6 = 10;
         char character = 'A';
         bool condition = false;
         byte number4 = 255;
@@ -16,6 +17,8 @@
         Console.WriteLine("Number3 is {0}", number3);
         Console.WriteLine("Number4 is {0}", number4);
         Console.WriteLine("Number5 is {0}", number5);
+        Console.WriteLine("Number6 is {0}", number6);
+
         Console.WriteLine("Character is : {0}", (int) character);
 
         Console.ReadLine();
